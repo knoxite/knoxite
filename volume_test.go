51d001b3dc54cef2bd66f4605cb6a3dca9f195ac
Christian Muehlhaeuser <muesli@gmail.com>
2016-07-30 01:09:58 +0200

    Added basic test cases for Volume

---
+++ volume_test.go
@@ -0,0 +1,68 @@
+/*
+ * knoxite
+ *     Copyright (c) 2016, Christian Muehlhaeuser <muesli@gmail.com>
+ *
+ *   For license see LICENSE.txt
+ */
+
+package knoxite
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestCreateVolume(t *testing.T) {
+	testPassword := "this_is_a_password"
+
+	dir, err := ioutil.TempDir("", "knoxite")
+	if err != nil {
+		t.Errorf("Failed creating temporary dir for repository: %s", err)
+		return
+	}
+	defer os.RemoveAll(dir)
+
+	vol, verr := NewVolume("test_name", "test_description")
+	{
+		r, err := NewRepository(dir, testPassword)
+		if err != nil {
+			t.Errorf("Failed creating repository: %s", err)
+			return
+		}
+
+		if verr == nil {
+			verr = r.AddVolume(vol)
+			if verr != nil {
+				t.Errorf("Failed creating volume: %s", verr)
+				return
+			}
+
+			serr := r.Save()
+			if serr != nil {
+				t.Errorf("Failed saving volume: %s", serr)
+				return
+			}
+		}
+	}
+
+	{
+		r, err := OpenRepository(dir, testPassword)
+		if err != nil {
+			t.Errorf("Failed opening repository: %s", err)
+			return
+		}
+
+		volume, err := r.FindVolume(vol.ID)
+		if err != nil {
+			t.Errorf("Failed finding volume: %s", err)
+			return
+		}
+		if volume.Name != vol.Name {
+			t.Errorf("Failed verifying volume name: %s != %s", vol.Name, volume.Name)
+		}
+		if volume.Description != vol.Description {
+			t.Errorf("Failed verifying volume description: %s != %s", vol.Description, volume.Description)
+		}
+	}
+}
