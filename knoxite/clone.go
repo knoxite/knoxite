51e2215e6c0a539376a9cedf45328d15f86a869c
Christian Muehlhaeuser <muesli@gmail.com>
2016-07-31 19:28:20 +0200

    knoxite can now clone existing snapshots with the clone command

---
+++ knoxite/clone.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+
+	"github.com/knoxite/knoxite"
+)
+
+// CmdClone describes the command
+type CmdClone struct {
+	store *CmdStore
+
+	global *GlobalOptions
+}
+
+func init() {
+	_, err := parser.AddCommand("clone",
+		"clone a snapshot",
+		"The clone command clones an existing snapshot and adds a file or directory",
+		&CmdClone{store: &CmdStore{}, global: &globalOpts})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Usage describes this command's usage help-text
+func (cmd CmdClone) Usage() string {
+	return "SNAPSHOT-ID DIR/FILE [DIR/FILE] [...]"
+}
+
+// Execute this command
+func (cmd CmdClone) Execute(args []string) error {
+	if len(args) < 2 {
+		return fmt.Errorf(TWrongNumArgs, cmd.Usage())
+	}
+	if cmd.global.Repo == "" {
+		return errors.New(TSpecifyRepoLocation)
+	}
+
+	targets := []string{}
+	for _, target := range args[1:] {
+		if absTarget, err := filepath.Abs(target); err == nil {
+			target = absTarget
+		}
+		targets = append(targets, target)
+	}
+
+	// filter here? exclude/include?
+
+	repository, err := knoxite.OpenRepository(cmd.global.Repo, cmd.global.Password)
+	if err != nil {
+		return err
+	}
+	volume, s, err := repository.FindSnapshot(args[0])
+	if err != nil {
+		return err
+	}
+	snapshot, err := s.Clone()
+	if err != nil {
+		return err
+	}
+	err = cmd.store.store(&repository, snapshot, targets)
+	if err != nil {
+		return err
+	}
+	err = snapshot.Save(&repository)
+	if err != nil {
+		return err
+	}
+	err = volume.AddSnapshot(snapshot.ID)
+	if err != nil {
+		return err
+	}
+	return repository.Save()
+}
