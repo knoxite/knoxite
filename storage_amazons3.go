2fb78ff7647bc6ad9217666547c6186726d1b9f8
Stefan Lücke <glaxx@glaxx.net>
2016-08-20 17:59:05 +0200

    Added skeleton for an Amazon S3 storage backend

---
+++ storage_amazons3.go
@@ -0,0 +1,68 @@
+/*
+ * knoxite
+ *     Copyright (c) 2016, Christian Muehlhaeuser <muesli@gmail.com>
+ *     Copyright (c) 2016, Stefan Luecke <glaxx@glaxx.net>
+ *   For license see LICENSE.txt
+ */
+
+package knoxite
+
+// StorageAmazonS3 stores data on a remote AmazonS3
+type StorageAmazonS3 struct {
+	URL string
+}
+
+// Location returns the type and location of the repository
+func (backend *StorageAmazonS3) Location() string {
+	return backend.URL
+}
+
+// Close the backend
+func (backend *StorageAmazonS3) Close() error {
+	return nil
+}
+
+// Protocols returns the Protocol Schemes supported by this backend
+func (backend *StorageAmazonS3) Protocols() []string {
+	return []string{"amazons3"}
+}
+
+// Description returns a user-friendly description for this backend
+func (backend *StorageAmazonS3) Description() string {
+	return "Amazon S3 Storage"
+}
+
+// LoadChunk loads a Chunk from network
+func (backend *StorageAmazonS3) LoadChunk(shasum string, part, totalParts uint) (*[]byte, error) {
+	return &[]byte{}, ErrChunkNotFound
+}
+
+// StoreChunk stores a single Chunk on network
+func (backend *StorageAmazonS3) StoreChunk(shasum string, part, totalParts uint, data *[]byte) (size uint64, err error) {
+	return 0, ErrStoreChunkFailed
+}
+
+// LoadSnapshot loads a snapshot
+func (backend *StorageAmazonS3) LoadSnapshot(id string) ([]byte, error) {
+	return []byte{}, ErrSnapshotNotFound
+}
+
+// SaveSnapshot stores a snapshot
+func (backend *StorageAmazonS3) SaveSnapshot(id string, data []byte) error {
+	return ErrStoreSnapshotFailed
+}
+
+// InitRepository creates a new repository
+func (backend *StorageAmazonS3) InitRepository() error {
+	return nil
+}
+
+// LoadRepository reads the metadata for a repository
+func (backend *StorageAmazonS3) LoadRepository() ([]byte, error) {
+	return []byte{}, ErrLoadRepositoryFailed
+}
+
+// SaveRepository stores the metadata for a repository
+func (backend *StorageAmazonS3) SaveRepository(data []byte) error {
+	return ErrStoreRepositoryFailed
+}
