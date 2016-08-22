36b29a189c9d45fa977cbfae217e56cbb1920b96
Stefan Lücke <glaxx@glaxx.net>
2016-08-22 17:51:52 +0200

    Concatenate the bucket strings once on init

--- storage_amazons3.go
+++ storage_amazons3.go
@@ -19,10 +19,12 @@ import (
 
 // StorageAmazonS3 stores data on a remote AmazonS3
 type StorageAmazonS3 struct {
-	url          url.URL
-	bucketPrefix string
-	region       string
-	client       *minio.Client
+	url              url.URL
+	chunkBucket      string
+	snapshotBucket   string
+	repositoryBucket string
+	region           string
+	client           *minio.Client
 }
 
 // NewStorageAmazonS3 returns a StorageAmazonS3 object.
@@ -50,9 +52,11 @@ func NewStorageAmazonS3(URL url.URL) (*StorageAmazonS3, error) {
 	}
 
 	return &StorageAmazonS3{url: URL,
-		client:       cl,
-		region:       regionAndBucketPrefix[1],
-		bucketPrefix: regionAndBucketPrefix[2],
+		client:           cl,
+		region:           regionAndBucketPrefix[1],
+		chunkBucket:      regionAndBucketPrefix[2] + "-chunks",
+		snapshotBucket:   regionAndBucketPrefix[2] + "-snapshots",
+		repositoryBucket: regionAndBucketPrefix[2] + "-repository",
 	}, nil
 }
 
@@ -79,7 +83,7 @@ func (backend *StorageAmazonS3) Description() string {
 // LoadChunk loads a Chunk from network
 func (backend *StorageAmazonS3) LoadChunk(shasum string, part, totalParts uint) (*[]byte, error) {
 	fileName := shasum + "." + strconv.FormatUint(uint64(part), 10) + "_" + strconv.FormatUint(uint64(totalParts), 10)
-	obj, err := backend.client.GetObject(backend.bucketPrefix+"-chunks", fileName)
+	obj, err := backend.client.GetObject(backend.chunkBucket, fileName)
 	if err != nil {
 		return nil, err
 	}
@@ -91,19 +95,19 @@ func (backend *StorageAmazonS3) LoadChunk(shasum string, part, totalParts uint) 
 func (backend *StorageAmazonS3) StoreChunk(shasum string, part, totalParts uint, data *[]byte) (size uint64, err error) {
 	fileName := shasum + "." + strconv.FormatUint(uint64(part), 10) + "_" + strconv.FormatUint(uint64(totalParts), 10)
 
-	if _, err := backend.client.StatObject(backend.bucketPrefix+"-chunks", fileName); err == nil {
+	if _, err := backend.client.StatObject(backend.chunkBucket, fileName); err == nil {
 		// Chunk is already stored
 		return 0, nil
 	}
 
 	buf := bytes.NewBuffer(*data)
-	i, err := backend.client.PutObject(backend.bucketPrefix+"-chunks", fileName, buf, "application/octet-stream")
+	i, err := backend.client.PutObject(backend.chunkBucket, fileName, buf, "application/octet-stream")
 	return uint64(i), err
 }
 
 // LoadSnapshot loads a snapshot
 func (backend *StorageAmazonS3) LoadSnapshot(id string) ([]byte, error) {
-	obj, err := backend.client.GetObject(backend.bucketPrefix+"-snapshots", id)
+	obj, err := backend.client.GetObject(backend.snapshotBucket, id)
 	if err != nil {
 		return nil, err
 	}
@@ -113,18 +117,18 @@ func (backend *StorageAmazonS3) LoadSnapshot(id string) ([]byte, error) {
 // SaveSnapshot stores a snapshot
 func (backend *StorageAmazonS3) SaveSnapshot(id string, data []byte) error {
 	buf := bytes.NewBuffer(data)
-	_, err := backend.client.PutObject(backend.bucketPrefix+"-snapshots", id, buf, "application/octet-stream")
+	_, err := backend.client.PutObject(backend.snapshotBucket, id, buf, "application/octet-stream")
 	return err
 }
 
 // InitRepository creates a new repository
 func (backend *StorageAmazonS3) InitRepository() error {
-	chunkBucketExist, err := backend.client.BucketExists(backend.bucketPrefix + "-chunks")
+	chunkBucketExist, err := backend.client.BucketExists(backend.chunkBucket)
 	if err != nil {
 		return err
 	}
 	if !chunkBucketExist {
-		err = backend.client.MakeBucket(backend.bucketPrefix+"-chunks", backend.region)
+		err = backend.client.MakeBucket(backend.chunkBucket, backend.region)
 		if err != nil {
 			return err
 		}
@@ -132,12 +136,12 @@ func (backend *StorageAmazonS3) InitRepository() error {
 		return ErrRepositoryExists
 	}
 
-	snapshotBucketExist, err := backend.client.BucketExists(backend.bucketPrefix + "-snapshots")
+	snapshotBucketExist, err := backend.client.BucketExists(backend.snapshotBucket)
 	if err != nil {
 		return err
 	}
 	if !snapshotBucketExist {
-		err = backend.client.MakeBucket(backend.bucketPrefix+"-snapshots", backend.region)
+		err = backend.client.MakeBucket(backend.snapshotBucket, backend.region)
 		if err != nil {
 			return err
 		}
@@ -145,12 +149,12 @@ func (backend *StorageAmazonS3) InitRepository() error {
 		return ErrRepositoryExists
 	}
 
-	repositoryBucketExist, err := backend.client.BucketExists(backend.bucketPrefix + "-repository")
+	repositoryBucketExist, err := backend.client.BucketExists(backend.repositoryBucket)
 	if err != nil {
 		return err
 	}
 	if !repositoryBucketExist {
-		err = backend.client.MakeBucket(backend.bucketPrefix+"-repository", backend.region)
+		err = backend.client.MakeBucket(backend.repositoryBucket, backend.region)
 		if err != nil {
 			return err
 		}
@@ -163,7 +167,7 @@ func (backend *StorageAmazonS3) InitRepository() error {
 
 // LoadRepository reads the metadata for a repository
 func (backend *StorageAmazonS3) LoadRepository() ([]byte, error) {
-	obj, err := backend.client.GetObject(backend.bucketPrefix+"-repository", repoFilename)
+	obj, err := backend.client.GetObject(backend.repositoryBucket, repoFilename)
 	if err != nil {
 		return nil, err
 	}
@@ -173,6 +177,6 @@ func (backend *StorageAmazonS3) LoadRepository() ([]byte, error) {
 // SaveRepository stores the metadata for a repository
 func (backend *StorageAmazonS3) SaveRepository(data []byte) error {
 	buf := bytes.NewBuffer(data)
-	_, err := backend.client.PutObject(backend.bucketPrefix+"-repository", repoFilename, buf, "application/octet-stream")
+	_, err := backend.client.PutObject(backend.repositoryBucket, repoFilename, buf, "application/octet-stream")
 	return err
 }
