8bf7ca990e8356823a08dbe2697d6695839527eb
Christian Muehlhaeuser <muesli@gmail.com>
2016-09-20 11:43:33 +0200

    Made Table's members private. Added SetSummary(string) to allow access to a Table's summary

--- knoxite/table.go
+++ knoxite/table.go
@@ -11,58 +11,69 @@ const timeFormat = "2006-01-02 15:04:05"
 
 // Table is a helper for printing data in sheet form
 type Table struct {
-	Headers   []string
-	Widths    []int64
-	Rows      [][]interface{}
-	Summary   []interface{}
-	EmptyText string
+	headers   []string
+	widths    []int64
+	rows      [][]interface{}
+	summary   []interface{}
+	emptyText string
 }
 
 // NewTable returns a new table
 func NewTable(headers []string, widths []int64, emptyText string) Table {
 	return Table{
-		Headers:   headers,
-		Widths:    widths,
-		Rows:      [][]interface{}{},
-		EmptyText: emptyText,
+		headers:   headers,
+		widths:    widths,
+		rows:      [][]interface{}{},
+		emptyText: emptyText,
+	}
+}
+
+func (t *Table) checkWidths(row []interface{}) {
+	for col, v := range row {
+		s := fmt.Sprint(v)
+		if float64(len(s)) > math.Abs(float64(t.widths[col])) {
+			// the added value doesn't fit in this column, let's make it bigger
+			l := int64(len(s))
+			if t.widths[col] < 0 {
+				l *= -1
+			}
+			t.widths[col] = l
+		}
 	}
 }
 
 // AppendRow adds a row to the end of the table
 func (t *Table) AppendRow(row []interface{}) {
-	for col, v := range row {
-		s := fmt.Sprint(v)
-		if float64(len(s)) > math.Abs(float64(t.Widths[col])) {
-			l := int64(len(s))
-			if t.Widths[col] < 0 {
-				l *= -1
-			}
-			t.Widths[col] = l
-		}
-	}
-	t.Rows = append(t.Rows, row)
+	t.checkWidths(row)
+	t.rows = append(t.rows, row)
+}
+
+// SetSummary sets a summary for this table, to be printed below the table
+func (t *Table) SetSummary(summary []interface{}) {
+	t.checkWidths(summary)
+	t.summary = summary
 }
 
 // Print writes the entire table to stdout
-func (t Table) Print() error {
+func (t *Table) Print() error {
 	totalWidth := int64(0)
 	format := ""
-	for _, w := range t.Widths {
+	for _, w := range t.widths {
 		format += "%" + strconv.FormatInt(w, 10) + "s  "
 		totalWidth += int64(math.Abs(float64(w))) + 2
 	}
 
 	// print header
-	fmt.Printf(format+"\n", ifaceify(t.Headers)...)
+	fmt.Printf(format+"\n", ifaceify(t.headers)...)
 	fmt.Println(strings.Repeat("-", int(totalWidth)))
 
 	// print rows
-	for _, row := range t.Rows {
+	for _, row := range t.rows {
 		fmt.Printf(format+"\n", row...)
 	}
-	if len(t.Rows) == 0 {
-		fmt.Println(t.EmptyText)
-	} else if len(t.Summary) > 0 {
+	if len(t.rows) == 0 {
+		fmt.Println(t.emptyText)
+	} else if len(t.summary) > 0 {
 		t.PrintSummary()
 	}
 
@@ -70,10 +81,10 @@ func (t Table) Print() error {
 }
 
 // PrintSummary writes the table summary to stdout
-func (t Table) PrintSummary() error {
+func (t *Table) PrintSummary() error {
 	totalWidth := int64(0)
 	format := ""
-	for _, w := range t.Widths {
+	for _, w := range t.widths {
 		format += "%" + strconv.FormatInt(w, 10) + "s  "
 		totalWidth += int64(math.Abs(float64(w))) + 2
 	}
@@ -82,7 +93,7 @@ func (t Table) PrintSummary() error {
 	fmt.Println(strings.Repeat("-", int(totalWidth)))
 
 	// print summary
-	fmt.Printf(format+"\n", t.Summary...)
+	fmt.Printf(format+"\n", t.summary...)
 
 	return nil
 }
