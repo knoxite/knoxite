d5b010929377abf1bfbce2ee700a959b3f616033
Christian Muehlhaeuser <muesli@gmail.com>
2016-12-11 15:32:33 +0100

    Added Progress.TransferSpeed() which returns the average transfer speed

--- progress.go
+++ progress.go
@@ -1,11 +1,14 @@
 package knoxite
 
+import "time"
+
 // Progress contains stats and current path
 type Progress struct {
 	Path        string
 	Size        uint64
 	StorageSize uint64
 	Transferred uint64
+	Timer       time.Time
 	Statistics  Stats
 	Error       error
 }
@@ -15,6 +18,7 @@ func newProgress(item *ItemData) Progress {
 		Path:        item.Path,
 		Size:        item.Size,
 		StorageSize: item.StorageSize,
+		Timer:       time.Now(),
 		Statistics:  Stats{},
 		Error:       nil,
 	}
@@ -24,4 +28,9 @@ func newProgressError(err error) Progress {
 	return Progress{
 		Error: err,
 	}
+}
+
+// TransferSpeed returns the average transfer speed in bytes per second
+func (p Progress) TransferSpeed() uint64 {
+	return uint64(float64(p.Transferred) / time.Since(p.Timer).Seconds())
 }
