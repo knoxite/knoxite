3dc08e14857430e7e3a3c2d0ba2df4efda74b091
Christian Muehlhaeuser <muesli@gmail.com>
2016-12-30 13:32:13 +0100

    Cleaned up redundancy.go

--- lib/redundancy.go
+++ lib/redundancy.go
@@ -2,13 +2,13 @@ package knoxite
 
 import "github.com/klauspost/reedsolomon"
 
-func redundantData(finalData []byte, chunks, redundancyChunks int) ([][]byte, error) {
+func redundantData(b []byte, chunks, redundancyChunks int) ([][]byte, error) {
 	enc, err := reedsolomon.New(chunks, redundancyChunks)
 	if err != nil {
 		return [][]byte{}, err
 	}
 
-	pardata, err := enc.Split(finalData)
+	pardata, err := enc.Split(b)
 	if err != nil {
 		panic(err)
 	}
