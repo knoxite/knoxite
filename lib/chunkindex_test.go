8713bae9c9220a28b310a73d06d21421bb266bcb
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-07 22:41:34 +0100

    Added test for reindexing ChunkIndex

--- lib/chunkindex_test.go
+++ lib/chunkindex_test.go
@@ -13,6 +13,45 @@ import (
 	"testing"
 )
 
+func TestChunkIndexReindex(t *testing.T) {
+	testPassword := "this_is_a_password"
+
+	dir, err := ioutil.TempDir("", "knoxite")
+	if err != nil {
+		t.Errorf("Failed creating temporary dir for repository: %s", err)
+		return
+	}
+	defer os.RemoveAll(dir)
+
+	r, _ := NewRepository(dir, testPassword)
+	vol, _ := NewVolume("test", "")
+	r.AddVolume(vol)
+
+	snapshot, _ := NewSnapshot("test_snapshot")
+	index, err := OpenChunkIndex(&r)
+	wd, err := os.Getwd()
+	progress := snapshot.Add(wd, []string{"snapshot_test.go", "snapshot.go"}, r, &index, false, true, 1, 0)
+	for p := range progress {
+		if p.Error != nil {
+			t.Errorf("Failed adding to snapshot: %s", p.Error)
+		}
+	}
+
+	snapshot.Save(&r)
+	vol.AddSnapshot(snapshot.ID)
+	index.Save(&r)
+	r.Save()
+
+	newindex, err := OpenChunkIndex(&r)
+	if err != nil {
+		t.Errorf("Failed reopening chunk-index: %s", err)
+	}
+	err = newindex.reindex(&r)
+	if err != nil {
+		t.Errorf("Failed reindexing chunk-index: %s", err)
+	}
+}
+
 func TestChunkIndexPack(t *testing.T) {
 	testPassword := "this_is_a_password"
 
