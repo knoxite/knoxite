260c277cf21586ce2f53cbfcb3941882c467677f
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-07 22:53:50 +0100

    This is a fishy test. I want to see it (mis-)behave

---
+++ lib/progress_test.go
@@ -0,0 +1,36 @@
+/*
+ * knoxite
+ *     Copyright (c) 2017, Christian Muehlhaeuser <muesli@gmail.com>
+ *
+ *   For license see LICENSE
+ */
+
+package knoxite
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestProgressTransferSpeed(t *testing.T) {
+	p := Progress{
+		Timer: time.Now(),
+		CurrentItemStats: Stats{
+			Transferred: 1024,
+		},
+	}
+
+	time.Sleep(1 * time.Second)
+	s := p.TransferSpeed()
+	if s != 1023 {
+		t.Errorf("Expected %d, got %d", 1023, s)
+	}
+}
+
+func TestProgressError(t *testing.T) {
+	p := newProgressError(errors.New("TestError"))
+	if p.Error == nil {
+		t.Errorf("Expected error, got %s", p.Error)
+	}
+}
