7497e0c19575810bd3331b167d6d7af70d36f307
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-08 22:39:46 +0100

    Fix pure-ftpd OSX behaviour on Travis

--- admin/setup_pureftpd_test_environment.sh
+++ admin/setup_pureftpd_test_environment.sh
@@ -7,8 +7,8 @@
 #
 
 # create ftp chroot
-sudo mkdir -p $HOME/knoxite-citest
-sudo chown -R $USER /$HOME/knoxite-citest
+sudo mkdir -p $HOME/ftp/knoxite-citest
+sudo chown -R $USER $HOME/ftp
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # install pure-ftpd
@@ -21,7 +21,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo sh -c "echo ',2121' > /etc/pure-ftpd/conf/Bind"
 
     # Create a password db from the passwd template
-    sudo sh -c "echo 'knoxite:\$1\$ONFUv0U0\$zLjMcFT8W7.mQelSHUp2b1:1000:1000::/home/travis/./::::::::::::' > /etc/pure-ftpd/pureftpd.passwd"
+    sudo sh -c "echo 'knoxite:\$1\$ONFUv0U0\$zLjMcFT8W7.mQelSHUp2b1:1000:1000::/home/travis/ftp/./::::::::::::' > /etc/pure-ftpd/pureftpd.passwd"
     sudo pure-pw mkdb
 
     sudo sh -c "echo '/etc/pure-ftpd/pureftpd.pdb' > /etc/pure-ftpd/conf/PureDB"
@@ -32,9 +32,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     # install pure-ftpd
     brew install pure-ftpd
 
+    sudo chmod -R a+rwx $HOME/ftp
+
     # Create a password db from the passwd template
-    echo 'knoxite:$7$C6..../....UHsuYTodjuMFgq.esCywrnooaUU7Lixu8TFb/ut9xj/$QjKkhpIhHwpWdRhEfvYdWS4pCio.qLMRqTDZ8yxAgZA:501:20::/Users/travis/./::::::::::::' > /tmp/pureftpd.passwd
+    echo 'knoxite:$7$C6..../....ahHkCWvmlfvbTtW2EUe.lW0b.RYkr5.9wL5pkBphre3$wFzTmcYizq91da1qnypUKGSVa7pvbC3vYvI2ZFsHNQ.:98:20::/Users/travis/ftp/./::::::::::::' > /tmp/pureftpd.passwd
     /usr/local/bin/pure-pw mkdb /tmp/pureftpd.pdb -f /tmp/pureftpd.passwd
 
-    /usr/local/sbin/pure-ftpd -d -B -S 127.0.0.1,2121 -l puredb:/tmp/pureftpd.pdb
+    sudo /usr/local/sbin/pure-ftpd -E -B -S 127.0.0.1,2121 --minuid=1 -l puredb:/tmp/pureftpd.pdb
 fi
