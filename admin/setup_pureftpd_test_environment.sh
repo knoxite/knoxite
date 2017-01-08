fe21294fa77ea5eb6e48482b2c5d304dccc94ea7
Christian Muehlhaeuser <muesli@gmail.com>
2017-01-08 19:28:38 +0100

    Cleaned up pureftpd test setup script

--- admin/setup_pureftpd_test_environment.sh
+++ admin/setup_pureftpd_test_environment.sh
@@ -5,28 +5,32 @@
 #
 #   For license see LICENSE
 #
+
+# create ftp chroot
+sudo mkdir -p $HOME/knoxite-citest
+sudo chown -R $USER /$HOME/knoxite-citest
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  sudo apt-get -qq update
-  sudo apt-get install -y pure-ftpd
+    # install pure-ftpd
+    sudo apt-get -qq update
+    sudo apt-get install -y pure-ftpd
 
-  sudo service pure-ftpd stop
+    sudo service pure-ftpd stop
 
-  sudo mkdir -p $HOME/knoxite-citest
-  sudo chown -R $USER /$HOME/knoxite-citest
+    # Create a password db from the passwd template
+    sudo cp admin/pureftpd.passwd /etc/pure-ftpd/
+    sudo pure-pw mkdb
 
-  sudo cp admin/pureftpd.passwd /etc/pure-ftpd/
-  sudo pure-pw mkdb
+    sudo sh -c "echo '/etc/pure-ftpd/pureftpd.pdb' > /etc/pure-ftpd/conf/PureDB"
+    sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/PureDB
 
-  sudo sh -c "echo '/etc/pure-ftpd/pureftpd.pdb' > /etc/pure-ftpd/conf/PureDB"
-  sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/PureDB
-
-  sudo service pure-ftpd start
+    sudo service pure-ftpd start
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew install pure-ftpd
+    # install pure-ftpd
+    brew install pure-ftpd
 
-  sudo mkdir -p $HOME/knoxite-citest
-  sudo chown -R $USER /$HOME/knoxite-citest
+    # Create a password db from the passwd template
+    /usr/local/bin/pure-pw mkdb /tmp/pureftpd.pdb -f admin/pureftpd.osx.passwd
 
-  /usr/local/bin/pure-pw mkdb /tmp/pureftpd.pdb -f admin/pureftpd.osx.passwd
-  sudo /usr/local/sbin/pure-ftpd -l puredb:/tmp/pureftpd.pdb -E -O clf:/tmp/pureftpd_transfer.log -B &
+    sudo /usr/local/sbin/pure-ftpd -l puredb:/tmp/pureftpd.pdb -E -O clf:/tmp/pureftpd_transfer.log -B &
 fi
