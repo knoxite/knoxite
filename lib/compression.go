ba8f20425f1d020ca28986ce33a29fa8ea86cbdc
Christian Muehlhaeuser <muesli@gmail.com>
2018-01-15 06:01:29 +0100

    Added generic compression interface and LZMA support

--- lib/compression.go
+++ lib/compression.go
@@ -10,32 +10,57 @@ package knoxite
 import (
 	"bytes"
 	"compress/gzip"
+	"io"
 	"io/ioutil"
+
+	"github.com/ulikunitz/xz"
 )
 
-// Which compression algo
+// Available compression algos
 const (
 	CompressionNone = iota
 	CompressionGZip
+	CompressionLZMA
 	CompressionLZW
 	CompressionFlate
 	CompressionZlib
 )
 
 // Compress data
-func Compress(b []byte) ([]byte, error) {
+func Compress(b []byte, compressionType uint16) ([]byte, error) {
 	var buf bytes.Buffer
-	w := gzip.NewWriter(&buf)
+	var w io.WriteCloser
+	var err error
+
+	switch compressionType {
+	case CompressionGZip:
+		w = gzip.NewWriter(&buf)
+	case CompressionLZMA:
+		w, err = xz.NewWriter(&buf)
+	}
+	if err != nil {
+		return []byte{}, err
+	}
 	w.Write(b)
 	w.Close()
-	b = buf.Bytes()
 
+	b = buf.Bytes()
 	return b, nil
 }
 
 // Uncompress data
-func Uncompress(b []byte) ([]byte, error) {
-	zr, err := gzip.NewReader(bytes.NewReader(b))
+func Uncompress(b []byte, compressionType uint16) ([]byte, error) {
+	var zr io.ReadCloser
+	var err error
+
+	switch compressionType {
+	case CompressionGZip:
+		zr, err = gzip.NewReader(bytes.NewReader(b))
+	case CompressionLZMA:
+		zri, erri := xz.NewReader(bytes.NewReader(b))
+		zr = ioutil.NopCloser(zri)
+		err = erri
+	}
 	if err != nil {
 		return []byte{}, err
 	}
