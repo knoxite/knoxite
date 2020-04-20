e4ac013e396fad2b0a3bf27d1e304d409087ecb7
Christian Muehlhaeuser <muesli@gmail.com>
2020-04-21 01:33:39 +0200

    Clean test scripts

--- admin/setup_minio_test_environment.sh
+++ admin/setup_minio_test_environment.sh
@@ -2,7 +2,7 @@
 #
 # knoxite
 #     Copyright (c) 2016, Stefan Luecke <glaxx@glaxx.net>
-#     Copyright (c) 2017, Christian Muehlhaeuser <muesli@gmail.com>
+#     Copyright (c) 2016-2020, Christian Muehlhaeuser <muesli@gmail.com>
 #
 #   For license see LICENSE
 #
@@ -16,10 +16,14 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     OS="darwin"
 fi
 
-curl https://dl.minio.io/server/minio/release/$OS-amd64/minio --output minio_test
+# download mino
+curl https://dl.minio.io/server/minio/release/${OS}-amd64/minio --output minio_test
 
+# create dirs
 chmod +x ./minio_test
 mkdir s3test
+
+# start minio
 ./minio_test server ./s3test &
 
 # wait for minio to boot up
