fae2248618457684fb5bffcaccad87d20ad1dc47
Christian Muehlhaeuser <muesli@gmail.com>
2020-04-21 01:25:02 +0200

    Add FTP tests to GitHub workflow

---
+++ admin/setup_ftp_test_environment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# knoxite
+#     Copyright (c) 2020, Christian Muehlhaeuser <muesli@gmail.com>
+#
+#   For license see LICENSE
+#
+
+FTPSERVER_VERSION="0.5"
+FTPSERVER_BINARY="/tmp/ftpserver"
+FTP_DIR="$HOME"/knoxite_ftp
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="darwin"
+fi
+
+# download ftpserver
+curl -L "https://github.com/fclairamb/ftpserver/releases/download/v${FTPSERVER_VERSION}/ftpserver-${OS}-amd64" --output ${FTPSERVER_BINARY}
+chmod +x ${FTPSERVER_BINARY}
+
+# create dirs
+mkdir -p "$FTP_DIR"
+
+# start FTP server
+/tmp/ftpserver -data "$FTP_DIR" &
+
+# wait for FTP server to boot up
+sleep 5
