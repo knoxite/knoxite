69a7648890d3ca95f6955c77a2a1f106204af32b
Christian Muehlhaeuser <muesli@gmail.com>
2020-04-25 22:23:31 +0200

    Add demo recording scripts

---
+++ admin/demos/demo-base.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+. ./demo-magic.sh
+
+PATH=$(pwd)/../../cmd/knoxite:$PATH
+#TYPE_SPEED=20
+NEWLINE=true
+DEMO_PROMPT="${GREEN}➜ ${CYAN}"
+
+# hide the evidence
+clear
---
+++ admin/demos/demo_tutorial.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+. ./demo-base.sh
+
+# prepare
+DEMO_REPO=/tmp/knoxite.demo
+rm -Rf "$DEMO_REPO"
+mkdir -p "$DEMO_REPO"
+cd "$DEMO_REPO"
+
+mkdir data
+head -c 1M </dev/urandom >data/important.txt
+head -c 1M </dev/urandom >data/stuff.txt
+
+# demo
+pe "mkdir backup"
+pe "knoxite -r backup/ -p password repo init"
+pe "export KNOXITE_REPOSITORY=backup/"
+pe "export KNOXITE_PASSWORD=password"
+pe "knoxite volume init \"Backups\" -d \"System backups\""
+pe "knoxite volume list"
+pe "knoxite store latest data/ -d \"Backup of my data\""
+pe "knoxite snapshot list latest"
+pe "knoxite ls latest"
+pe "mkdir restore"
+pe "knoxite restore latest restore/"
+p ""
+
+# cleanup
+rm -Rf "$DEMO_REPO"
