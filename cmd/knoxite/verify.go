83a6a17c5309511d0e9c35ef6fe1973111014c65
Fabian Siegel <fabians1999@gmail.com>
2020-06-01 21:49:23 +0200

    #69 added verify for whole volumes

--- cmd/knoxite/verify.go
+++ cmd/knoxite/verify.go
@@ -30,7 +30,7 @@ var (
 			if len(args) == 0 {
 				return executeVerifyRepo()
 			} else if len(args) == 1 {
-				return executeVerifyVolume(args[0])
+				return executeVerifyVolume(args[0], verifyOpts)
 			} else if len(args) == 2 {
 				return executeVerifySnapshot(args[0], args[1], verifyOpts)
 			}
@@ -53,9 +53,49 @@ func executeVerifyRepo() error {
 	return nil
 }
 
-func executeVerifyVolume(volumeId string) error {
+func executeVerifyVolume(volumeId string, opts VerifyOptions) error {
+	errors := make([]error, 0)
+	repository, err := openRepository(globalOpts.Repo, globalOpts.Password)
+	if err == nil {
+		progress, err := knoxite.VerifyVolume(repository, volumeId, opts.Percentage)
+		if err != nil {
+			errors = append(errors, err)
+			return err
+		}
 
-	return nil
+		pb := &goprogressbar.ProgressBar{Total: 1000, Width: 40}
+		lastPath := ""
+	
+
+		for p := range progress {
+			if p.Error != nil {
+				fmt.Println()
+				errors = append(errors, p.Error)
+			}
+
+			pb.Total = int64(p.CurrentItemStats.Size)
+			pb.Current = int64(p.CurrentItemStats.Transferred)
+			pb.PrependText = fmt.Sprintf("%s / %s",
+				knoxite.SizeToString(uint64(pb.Current)),
+				knoxite.SizeToString(uint64(pb.Total)))
+
+			if p.Path != lastPath {
+				// We have just started restoring a new item
+				if len(lastPath) > 0 {
+					fmt.Println()
+				}
+				lastPath = p.Path
+				pb.Text = p.Path
+			}
+
+			pb.LazyPrint()
+
+		}
+		fmt.Println()
+		fmt.Printf("Verify done: %d errors\n", len(errors))
+		return nil
+	}
+	return err
 }
 
 func executeVerifySnapshot(volumeId string, snapshotId string, opts VerifyOptions) error {
--- verify.go
+++ verify.go
@@ -12,6 +12,74 @@ import (
 	"math/rand"
 )
 
+
+func VerifyVolume(repository Repository, volumeId string, Percentage int) (prog chan Progress, err error) {
+    prog = make(chan Progress)
+
+	go func() {
+		volume, ferr := repository.FindVolume(volumeId)
+		if ferr != nil {
+		    prog <- newProgressError(ferr)
+		}
+
+
+		archiveToSnapshot := make(map[string]*Snapshot)
+		
+		for _, snapshotHash := range volume.Snapshots {
+		    _, snapshot, err := repository.FindSnapshot(snapshotHash)
+		    if err != nil {
+			prog <- newProgressError(err)
+		    }
+
+		    for archiveHash := range snapshot.Archives {
+			archiveToSnapshot[archiveHash] = snapshot
+		    }
+			
+		}
+
+		archives := make([]string, 0)
+		for archiveHash := range archiveToSnapshot {
+		    archives= append(archives, archiveHash)
+		}
+
+		// get all keys of the snapshot Archives
+
+		if Percentage > 100 {
+			Percentage = 100
+		} else if Percentage < 0 {
+			Percentage = 0
+		}
+
+		// select len(keys)*percentage unique keys to verify
+		nrOfSelectedArchives := int(math.Ceil(float64(len(archives)*Percentage) / 100.0))
+
+		// we use a map[string]bool as a Set implementation
+		selectedArchives := make(map[string]bool)
+		for len(selectedArchives) < nrOfSelectedArchives {
+			idx := rand.Intn(len(archives))
+			selectedArchives[archives[idx]] = true
+		}
+
+		for archiveKey := range selectedArchives {
+			snapshot := archiveToSnapshot[archiveKey]
+			p := newProgress(snapshot.Archives[archiveKey])
+			prog <- p
+
+			err := VerifyArchive(repository, *snapshot.Archives[archiveKey])
+			if err != nil {
+				prog <- newProgressError(err)
+			}
+
+			p.CurrentItemStats.Transferred += uint64((*snapshot.Archives[archiveKey]).Size)
+			prog <- p
+
+		}
+		close(prog)
+	}()
+
+	return prog, nil
+}
+
 func VerifySnapshot(repository Repository, snapshotId string, Percentage int) (prog chan Progress, err error) {
 	prog = make(chan Progress)
 
