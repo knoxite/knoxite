79933e48c786f2ebd8678b5f4bd9ddb3db66255d
Fabian Siegel <fabians1999@gmail.com>
2020-06-03 23:13:13 +0200

    Added verify command (#119)

---
+++ verify.go
@@ -0,0 +1,207 @@
+/*
+ * knoxite
+ *     Copyright (c) 2020, Fabian Siegel <fabians1999@gmail.com>
+ *
+ *   For license see LICENSE
+ */
+
+package knoxite
+
+import (
+	"math"
+	"math/rand"
+)
+
+func VerifyRepo(repository Repository, percentage int) (prog chan Progress, err error) {
+	prog = make(chan Progress)
+
+	go func() {
+		archiveToSnapshot := make(map[string]*Snapshot)
+
+		for _, volume := range repository.Volumes {
+			for _, snapshotHash := range volume.Snapshots {
+				_, snapshot, err := repository.FindSnapshot(snapshotHash)
+				if err != nil {
+					prog <- newProgressError(err)
+				}
+
+				for archiveHash := range snapshot.Archives {
+					archiveToSnapshot[archiveHash] = snapshot
+				}
+			}
+		}
+
+		archives := make([]string, 0)
+		for archiveHash := range archiveToSnapshot {
+			archives = append(archives, archiveHash)
+		}
+
+		// get all keys of the snapshot Archives
+		if percentage > 100 {
+			percentage = 100
+		} else if percentage < 0 {
+			percentage = 0
+		}
+
+		// select len(keys)*percentage unique keys to verify
+		nrOfSelectedArchives := int(math.Ceil(float64(len(archives)*percentage) / 100.0))
+
+		// we use a map[string]bool as a Set implementation
+		selectedArchives := make(map[string]bool)
+		for len(selectedArchives) < nrOfSelectedArchives {
+			idx := rand.Intn(len(archives))
+			selectedArchives[archives[idx]] = true
+		}
+
+		for archiveKey := range selectedArchives {
+			snapshot := archiveToSnapshot[archiveKey]
+			p := newProgress(snapshot.Archives[archiveKey])
+			prog <- p
+
+			err := VerifyArchive(repository, *snapshot.Archives[archiveKey])
+			if err != nil {
+				prog <- newProgressError(err)
+			}
+
+			p.CurrentItemStats.Transferred += uint64((*snapshot.Archives[archiveKey]).Size)
+			prog <- p
+
+		}
+		close(prog)
+	}()
+
+	return prog, nil
+}
+
+func VerifyVolume(repository Repository, volumeId string, percentage int) (prog chan Progress, err error) {
+	prog = make(chan Progress)
+
+	go func() {
+		volume, ferr := repository.FindVolume(volumeId)
+		if ferr != nil {
+			prog <- newProgressError(ferr)
+		}
+
+		archiveToSnapshot := make(map[string]*Snapshot)
+
+		for _, snapshotHash := range volume.Snapshots {
+			_, snapshot, err := repository.FindSnapshot(snapshotHash)
+			if err != nil {
+				prog <- newProgressError(err)
+			}
+
+			for archiveHash := range snapshot.Archives {
+				archiveToSnapshot[archiveHash] = snapshot
+			}
+		}
+
+		archives := make([]string, 0)
+		for archiveHash := range archiveToSnapshot {
+			archives = append(archives, archiveHash)
+		}
+
+		// get all keys of the snapshot Archives
+		if percentage > 100 {
+			percentage = 100
+		} else if percentage < 0 {
+			percentage = 0
+		}
+
+		// select len(keys)*percentage unique keys to verify
+		nrOfSelectedArchives := int(math.Ceil(float64(len(archives)*percentage) / 100.0))
+
+		// we use a map[string]bool as a Set implementation
+		selectedArchives := make(map[string]bool)
+		for len(selectedArchives) < nrOfSelectedArchives {
+			idx := rand.Intn(len(archives))
+			selectedArchives[archives[idx]] = true
+		}
+
+		for archiveKey := range selectedArchives {
+			snapshot := archiveToSnapshot[archiveKey]
+			p := newProgress(snapshot.Archives[archiveKey])
+			prog <- p
+
+			err := VerifyArchive(repository, *snapshot.Archives[archiveKey])
+			if err != nil {
+				prog <- newProgressError(err)
+			}
+
+			p.CurrentItemStats.Transferred += uint64((*snapshot.Archives[archiveKey]).Size)
+			prog <- p
+		}
+		close(prog)
+	}()
+
+	return prog, nil
+}
+
+func VerifySnapshot(repository Repository, snapshotId string, percentage int) (prog chan Progress, err error) {
+	prog = make(chan Progress)
+
+	go func() {
+		_, snapshot, ferr := repository.FindSnapshot(snapshotId)
+		if ferr != nil {
+			prog <- newProgressError(ferr)
+		}
+
+		// get all keys of the snapshot Archives
+		archives := make([]string, 0, len(snapshot.Archives))
+		for key := range snapshot.Archives {
+			archives = append(archives, key)
+		}
+
+		if percentage > 100 {
+			percentage = 100
+		} else if percentage < 0 {
+			percentage = 0
+		}
+
+		// select len(keys)*percentage unique keys to verify
+		nrOfSelectedArchives := int(math.Ceil(float64(len(archives)*percentage) / 100.0))
+
+		// we use a map[string]bool as a Set implementation
+		selectedArchives := make(map[string]bool)
+		for len(selectedArchives) < nrOfSelectedArchives {
+			idx := rand.Intn(len(archives))
+			selectedArchives[archives[idx]] = true
+		}
+
+		for archiveKey := range selectedArchives {
+			p := newProgress(snapshot.Archives[archiveKey])
+			prog <- p
+
+			err := VerifyArchive(repository, *snapshot.Archives[archiveKey])
+			if err != nil {
+				prog <- newProgressError(err)
+			}
+
+			p.CurrentItemStats.Transferred += uint64((*snapshot.Archives[archiveKey]).Size)
+			prog <- p
+
+		}
+		close(prog)
+	}()
+
+	return prog, nil
+}
+
+func VerifyArchive(repository Repository, arc Archive) error {
+	if arc.Type == File {
+		parts := uint(len(arc.Chunks))
+		for i := uint(0); i < parts; i++ {
+			idx, erri := arc.IndexOfChunk(i)
+			if erri != nil {
+				return erri
+			}
+
+			chunk := arc.Chunks[idx]
+			_, errc := loadChunk(repository, arc, chunk)
+			if errc != nil {
+				return errc
+			}
+		}
+		return nil
+	}
+	return nil
+}
