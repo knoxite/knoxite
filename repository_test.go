9f3beb13ef0a2bb69ddd26ccf63aadc57700ac96
mahartma <mahartma@mahartma.com>
2020-06-12 22:43:33 +0200

    Tests for repository passwd

--- repository_test.go
+++ repository_test.go
@@ -73,3 +73,45 @@ func TestRepositoryIsEmpty(t *testing.T) {
 		t.Error("Repository should not be empty")
 	}
 }
+
+func TestRepositoryChangePassword(t *testing.T) {
+	testPassword := "this_is_a_password"
+	newPassword := "this_is_another_password"
+
+	dir, err := ioutil.TempDir("", "knoxite")
+	if err != nil {
+		t.Errorf("Failed creating temporary dir for repository: %s", err)
+		return
+	}
+	defer os.RemoveAll(dir)
+
+	_, err = NewRepository(dir, testPassword)
+	if err != nil {
+		t.Errorf("Failed creating repository: %s", err)
+		return
+	}
+
+	repo, err := OpenRepository(dir, testPassword)
+	if err != nil {
+		t.Errorf("Failed opening repository: %s", err)
+		return
+	}
+
+	if repo.ChangePassword(newPassword) != nil {
+		t.Errorf("Failed to change repository password: %s", err)
+		return
+	}
+
+	_, err = OpenRepository(dir, testPassword)
+	if err == nil {
+		t.Errorf("Repository can still be opened with the old password after changing it: %s", err)
+		return
+	}
+
+	_, err = OpenRepository(dir, newPassword)
+	if err != nil {
+		t.Errorf("Failed opening repository with new password after changing it: %s", err)
+		return
+	}
+
+}
