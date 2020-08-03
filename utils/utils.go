3ec93d86a5db1ec8fa0b9c66c9b511544e920d94
Nicolas Martin <penguwin@penguwin.eu>
2020-08-03 13:11:24 +0200

    Move utility functions for compression from store.go to utils.go
    - Move CompressionTypeFromString() to utils.go
    - Move CompressionText to utils.go
    - Also move ErrCompressionUnkown to utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -7,3 +7,56 @@
  */
 
 package utils
+
+import (
+	"errors"
+	"strings"
+	"github.com/knoxite/knoxite"
+)
+
+var (
+	ErrCompressionUnknown = errors.New("unknown compression format")
+)
+// CompressionTypeFromString returns the compression type from a user-specified string
+func CompressionTypeFromString(s string) (uint16, error) {
+	switch strings.ToLower(s) {
+	case "":
+		// default is none
+		fallthrough
+	case "none":
+		return knoxite.CompressionNone, nil
+	case "flate":
+		return knoxite.CompressionFlate, nil
+	case "gzip":
+		return knoxite.CompressionGZip, nil
+	case "lzma":
+		return knoxite.CompressionLZMA, nil
+	case "zlib":
+		return knoxite.CompressionZlib, nil
+	case "zstd":
+		return knoxite.CompressionZstd, nil
+	}
+
+	return 0, ErrCompressionUnknown
+}
+
+// CompressionText returns a user-friendly string indicating the compression algo that was used
+// returns "unknown" when none is found
+func CompressionText(enum int) string {
+	switch enum {
+	case knoxite.CompressionNone:
+		return "none"
+	case knoxite.CompressionFlate:
+		return "Flate"
+	case knoxite.CompressionGZip:
+		return "GZip"
+	case knoxite.CompressionLZMA:
+		return "LZMA"
+	case knoxite.CompressionZlib:
+		return "zlib"
+	case knoxite.CompressionZstd:
+		return "zstd"
+	}
+
+	return "unknown"
+}
