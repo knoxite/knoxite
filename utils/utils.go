3fe935c2a95779212806f24250e8e70d5f81cb83
Nicolas Martin <penguwin@penguwin.eu>
2020-08-03 13:02:57 +0200

    Create a utils/utils.go file for commonly used methods

---
+++ utils/utils.go
@@ -0,0 +1,9 @@
+/*
+ * knoxite
+ *     Copyright (c) 2020, Christian Muehlhaeuser <muesli@gmail.com>
+ *     Copyright (c) 2020, Nicolas Martin <penguwin@penguwin.eu>
+ *
+ *   For license see LICENSE
+ */
+
+package utils
