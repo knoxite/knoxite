12b6896db4990ae08fa4e3a4a3d0ff237d400039
mahartma <mahartma@mahartma.com>
2020-09-20 06:02:45 +0200

    Adds verbose logging messages to snapshot.go

--- cmd/knoxite/snapshot.go
+++ cmd/knoxite/snapshot.go
@@ -54,35 +54,50 @@ func init() {
 }
 
 func executeSnapshotRemove(snapshotID string) error {
+	logger.Info("Opening repository")
 	repository, err := openRepository(globalOpts.Repo, globalOpts.Password)
 	if err != nil {
 		return err
 	}
+	logger.Info("Opened repository")
+
+	logger.Info("Opening chunk index")
 	chunkIndex, err := knoxite.OpenChunkIndex(&repository)
 	if err != nil {
 		return err
 	}
+	logger.Info("Opened chunk index")
 
+	logger.Info(fmt.Sprintf("Finding snapshot %s", snapshotID))
 	volume, snapshot, err := repository.FindSnapshot(snapshotID)
 	if err != nil {
 		return err
 	}
+	logger.Info("Found snapshot")
 
+	logger.Info(fmt.Sprintf("Removing snapshot %s. Description: %s. Date: %s", snapshotID, snapshot.Description, snapshot.Date))
 	err = volume.RemoveSnapshot(snapshot.ID)
 	if err != nil {
 		return err
 	}
+	logger.Info("Removed snapshot")
 
+	logger.Info(fmt.Sprintf("Removing snapshot %s from chunk index", snapshotID))
 	chunkIndex.RemoveSnapshot(snapshot.ID)
+
+	logger.Info("Saving chunk index for repository")
 	err = chunkIndex.Save(&repository)
 	if err != nil {
 		return err
 	}
+	logger.Info("Saved chunk index")
 
+	logger.Info("Saving repository")
 	err = repository.Save()
 	if err != nil {
 		return err
 	}
+	logger.Info("Saved repository")
 
 	fmt.Printf("Snapshot %s removed: %s\n", snapshot.ID, snapshot.Stats.String())
 	fmt.Println("Do not forget to run 'repo pack' to delete un-referenced chunks and free up storage space!")
@@ -90,26 +105,36 @@ func executeSnapshotRemove(snapshotID string) error {
 }
 
 func executeSnapshotList(volID string) error {
+	logger.Info("Opening repository")
 	repository, err := openRepository(globalOpts.Repo, globalOpts.Password)
 	if err != nil {
 		return err
 	}
+	logger.Info("Opened repository")
 
+	logger.Info(fmt.Sprintf("Finding volume %s", volID))
 	volume, err := repository.FindVolume(volID)
 	if err != nil {
 		return err
 	}
+	logger.Info("Found volume")
 
+	logger.Debug("Initializing new gotable for output")
 	tab := gotable.NewTable([]string{"ID", "Date", "Original Size", "Storage Size", "Description"},
 		[]int64{-8, -19, 13, 12, -48}, "No snapshots found. This volume is empty.")
 	totalSize := uint64(0)
 	totalStorageSize := uint64(0)
 
+	logger.Debug("Iterating over snapshots to print details")
 	for _, snapshotID := range volume.Snapshots {
+		logger.Debug(fmt.Sprintf("Loading snapshot %s", snapshotID))
 		snapshot, err := volume.LoadSnapshot(snapshotID, &repository)
 		if err != nil {
 			return err
 		}
+		logger.Debug("Loaded snapshot")
+
+		logger.Debug("Appending snapshot information to gotable")
 		tab.AppendRow([]interface{}{
 			snapshot.ID,
 			snapshot.Date.Format(timeFormat),
@@ -121,6 +146,12 @@ func executeSnapshotList(volID string) error {
 	}
 
 	tab.SetSummary([]interface{}{"", "", knoxite.SizeToString(totalSize), knoxite.SizeToString(totalStorageSize), ""})
-	_ = tab.Print()
+
+	logger.Debug("Printing snapshot list output")
+	err = tab.Print()
+	if err != nil {
+		return err
+	}
+	logger.Debug("Printed output")
 	return nil
 }
