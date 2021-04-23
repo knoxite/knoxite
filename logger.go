3960c392cf64641f27f441944e2410a62176c7fa
mahartma <mahartma@mahartma.com>
2021-04-23 22:35:10 +0200

    Adds NopLogger to use if no Logger provided via SetLogger()

--- logger.go
+++ logger.go
@@ -7,7 +7,7 @@
 
 package knoxite
 
-// Logger is used for levelled logging and verbose flag
+// Logger is used for levelled logging and verbose flag.
 type Logger interface {
 	Fatal(v ...interface{})
 	Fatalf(format string, v ...interface{})
@@ -18,3 +18,31 @@ type Logger interface {
 	Debug(v ...interface{})
 	Debugf(format string, v ...interface{})
 }
+
+var (
+	log Logger = NopLogger{}
+)
+
+func SetLogger(l Logger) {
+	log = l
+}
+
+// NopLogger will be used by default if no logger has been set via SetLogger().
+type NopLogger struct {
+}
+
+func (nl NopLogger) Warn(v ...interface{}) {}
+
+func (nl NopLogger) Warnf(format string, v ...interface{}) {}
+
+func (nl NopLogger) Info(v ...interface{}) {}
+
+func (nl NopLogger) Infof(format string, v ...interface{}) {}
+
+func (nl NopLogger) Debug(v ...interface{}) {}
+
+func (nl NopLogger) Debugf(format string, v ...interface{}) {}
+
+func (nl NopLogger) Fatal(v ...interface{}) {}
+
+func (nl NopLogger) Fatalf(format string, v ...interface{}) {}
